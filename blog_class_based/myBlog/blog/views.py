4d7b1149cf96faf9f182e9ebd1f2ce29cbe9533d
Tarun Goel <tarun.goel@velsof.com>
2026-03-23 15:03:15 +0530

    Blog Website using Class based views

--- blog_class_based/myBlog/blog/views.py
+++ blog_class_based/myBlog/blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
-# Create your views here.
+#list view
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name='posts'
+    
+#detail view
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name='post'
+    
+#create view
+class PostCreateView(CreateView):
+    model = Post
+    template_name='blog/post_form.html'
+    fields = ['title','content']
+    
+#update view 
+class PostUpdateView(UpdateView):
+    model =Post
+    template_name='blog/post_form.html'
+    fields= ['title','content']
+    
+#delete view
+class PostDeleteView(DeleteView):
+    model=Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
