efcfa1b766937855baf0682a70fbd0ee4dbd35eb
Tarun Goel <tarun.goel@velsof.com>
2026-03-24 16:59:14 +0530

    Calculator-> Fix 2

--- calculator.py
+++ calculator.py
@@ -1,22 +1,22 @@
 def main():
-    operation = input("Enter the operation you want to perform ")
-    a = int(input("Enter first number "))
-    b = int(input("Enter second number "))
-    if(operation == '+'):
-        print(f"{a} {operation} {b} => {a+b}") 
-    elif(operation == "-"):
-        print(f"{a} {operation} {b} => {a-b}")
-    elif(operation == "*"):
-        print(f"{a} {operation} {b} => {a*b}")
-    elif(operation == "/"):
+    operation = input('Enter the operation you want to perform ')
+    a = int(input('Enter first number '))
+    b = int(input('Enter second number '))
+    if operation == '+':
+        print(f'{a} {operation} {b} => {a+b}') 
+    elif operation == '-':
+        print(f'{a} {operation} {b} => {a-b}')
+    elif operation == '*':
+        print(f'{a} {operation} {b} => {a*b}')
+    elif operation == '/':
         if b == 0:
-            print("Can not divide by zero")
+            print('Can not divide by zero')
         else:
-            print(f"{a} {operation} {b} => {a/b}") 
+            print(f'{a} {operation} {b} => {a/b}') 
     else:
-        print("Enter valid input")  
+        print('Enter valid input')  
         
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
 
