bbc2a0e7932f46c92a9429347bc34866f2ae272d
Tarun Goel <tarun.goel@velsof.com>
2026-03-24 16:54:54 +0530

    Calculator ->Fix-1

--- Django_Rest_Api/myproject/api/urls.py
+++ Django_Rest_Api/myproject/api/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('students/add/',views.add_student,name='add_student'),
     path('students/update/<int:pk>/',views.update_student,name='update_student'),
     path('students/delete/<int:pk>/',views.delete_student,name='delete_student')
-
 ]
--- calculator.py
+++ calculator.py
@@ -1,17 +1,22 @@
-operation = input("Enter the operation you want to perform ")
-a = int(input("Enter first number "))
-b = int(input("Enter second number "))
-if(operation == '+'):
-     print(f"{a} {operation} {b} => {a+b}") 
-elif(operation == "-"):
-    print(f"{a} {operation} {b} => {a-b}")
-elif(operation == "*"):
-    print(f"{a} {operation} {b} => {a*b}")
-elif(operation == "/"):
-    if b == 0:
-        print("Can not divide by zero")
+def main():
+    operation = input("Enter the operation you want to perform ")
+    a = int(input("Enter first number "))
+    b = int(input("Enter second number "))
+    if(operation == '+'):
+        print(f"{a} {operation} {b} => {a+b}") 
+    elif(operation == "-"):
+        print(f"{a} {operation} {b} => {a-b}")
+    elif(operation == "*"):
+        print(f"{a} {operation} {b} => {a*b}")
+    elif(operation == "/"):
+        if b == 0:
+            print("Can not divide by zero")
+        else:
+            print(f"{a} {operation} {b} => {a/b}") 
     else:
-        print(f"{a} {operation} {b} => {a/b}") 
-else:
-    print("Enter valid input")     
+        print("Enter valid input")  
+        
+if __name__ == "__main__":
+    main()
+
 
