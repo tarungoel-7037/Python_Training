5cd759ae037d5be012e39e3c4d03ce56358e79ac
Tarun Goel <tarun.goel@velsof.com>
2026-03-24 17:12:32 +0530

    File Handling -> Fix 1

--- file_handling/main.py
+++ file_handling/main.py
@@ -1,8 +1,12 @@
 import re
+from pathlib import Path
 
+BASE_DIR = Path(__file__).parent
 log_pattern = r'(\d{1,3}(?:\.\d{1,3}){3}) - - \[([^\]]+)\] ".*?" (\d{3}) \d+'
+input_path = BASE_DIR /  'sample_log.log'
+output_path = BASE_DIR /  'output.csv'
 
-with open("file_handling/sample_log.log") as f,open("file_handling/output.csv","w") as o:
+with input_path.open() as f, output_path.open('w') as o:
     o.write("IP Address, Date/Time, Status Code\n")
     
     for line in f:
