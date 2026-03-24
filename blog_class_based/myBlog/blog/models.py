a609fcad9bd370fc4f1441aa42943e9799b4e7c3
Tarun Goel <tarun.goel@velsof.com>
2026-03-24 17:48:46 +0530

    Blog Class Based View ->Fix 2

--- blog_class_based/myBlog/blog/models.py
+++ blog_class_based/myBlog/blog/models.py
@@ -3,12 +3,12 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length = 200)
     content  = models.TextField()
     
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('post_detail',args=[str(self.id)])
+        return reverse('post_detail', args=[str(self.id)])
         
--- blog_class_based/myBlog/blog/views.py
+++ blog_class_based/myBlog/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
 
@@ -6,28 +6,28 @@ from .models import Post
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
-    context_object_name='posts'
+    context_object_name ='posts'
     
 #detail view
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name='post'
+    context_object_name ='post'
     
 #create view
 class PostCreateView(CreateView):
     model = Post
-    template_name='blog/post_form.html'
+    template_name ='blog/post_form.html'
     fields = ['title','content']
     
 #update view 
 class PostUpdateView(UpdateView):
-    model =Post
-    template_name='blog/post_form.html'
-    fields= ['title','content']
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title','content']
     
 #delete view
 class PostDeleteView(DeleteView):
-    model=Post
+    model = Post
     template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('post_list')
